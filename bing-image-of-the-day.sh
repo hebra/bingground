5edfff664ebf2b31ea224d44d63dcfb2046025ff
Hendrik Brandt <hbrandt@intershop.com>
2016-09-06 10:46:41 +1000

    Initial script

---
+++ bing-image-of-the-day.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# This script retrieves the Microsoft Bing! picture of the day and sets it as background image.
+#
+# (C) 2016 Hendrik Brandt
+#
+# Released under term of Simplified BSD License
+#
+
+imgFolder=~/.local/share/bingPirctureOfTheDay
+mkdir -p $imgFolder
+
+imgJSON=$(curl -s "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-AU")
+
+imgPath=$(echo "$imgJSON" | jq -r  '.["images"][0].url')
+
+imgFile=$(basename $imgPath)
+
+imgURL="http://www.bing.com$imgPath"
+
+curl -o $imgFolder/$imgFile -s $imgURL
+
+gsettings set org.gnome.desktop.background picture-uri "file://$imgFolder/$imgFile"
